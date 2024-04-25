81d1b1094ee186849dfb9c673353322a1a371ab0
jonnhyOrtega <jonnhyortega@gmail.com>
2024-04-25 12:18:21 -0300

    Casi finalizado

--- src/conversorDeMoneda/Main.java
+++ src/conversorDeMoneda/Main.java
@@ -19,9 +19,13 @@ public class Main {
         String exit = "SALIR";
 
         while (true) {
+            String x = "empty";
+            String y = "empty";
+            double z = 0;
 
             System.out.println("""
-                    Ingrese la divisa que desea convertir o SALIR para salir del programa:
+                    Ingrese la divisa que desea convertir o escriba SALIR para finalzar:
+                    
                     EUR - Euros
                     USD - Dolares
                     ARS - Pesos argentinos
@@ -38,76 +42,60 @@ public class Main {
                 break;
             }
             else {
-                var x = currencyToConvert;
+                x = currencyToConvert;
                 System.out.println("""
-                    Ingrese la divisa por la cual desea realizar la conversion o escriba SALIR finalizar:
+                    Ahora ingrese la divisa por la cual desea realizar la conversion o escriba SALIR para finalizar:
+                    
                     EUR - Euros
                     USD - Dolares
                     ARS - Pesos argentinos
                     BRL - Reales brasileños
                     """);
-                Scanner inputUserSecond = new Scanner(System.in);
-                String changeDivisa = inputUserSecond.nextLine();
+                String changeDivisa = inputUser.nextLine();
                 if (Objects.equals(changeDivisa, exit)) {
                     System.out.println("MUCHAS GRACIAS POR USAR https://www.exchangerate-api.com/docs/pair-conversion-requests NOS VEMOS PRONTO");
                     break;
                 }
                 else{
-                    var y = changeDivisa;
+                    y = changeDivisa;
 
                     System.out.println("""
-                    Ingrese el valor que desea consultar o 9.0 para salir:
+                    Ingrese el valor que desea consultar o 9 para salir:
                     """);
-                    Scanner inputUserThird = new Scanner(System.in);
-                    double  valueUser = inputUserThird.nextDouble();
-                    double exitCopy = 9.0;
+                    double  valueUser = inputUser.nextDouble();
+                    double exitCopy = 9;
                     if (Objects.equals(valueUser, exitCopy)) {
                         System.out.println("MUCHAS GRACIAS POR USAR https://www.exchangerate-api.com/docs/pair-conversion-requests NOS VEMOS PRONTO");
                         break;
                     }
                     else {
-                        var z = valueUser;
-
-
-                        // Setting URL
-                        String data = "https://v6.exchangerate-api.com/v6/3d3a8c46ed3aa968aa323588/pair/" + x + "/" + y + "/" + z;
-
-                        HttpClient client = HttpClient.newHttpClient();
-                        HttpRequest request = HttpRequest.newBuilder()
-                                .uri(URI.create(data))
-                                .build();
-
-                        HttpResponse<String> response = client
-                                .send(request, HttpResponse.BodyHandlers.ofString());
-
-                        // CODIFICACION DE DATOS
-                        String json = response.body();
-                        Gson gson = new Gson();
-                        Conversion conversion = gson.fromJson(json, Conversion.class);
-                        System.out.println("Valor convertido " + conversion.getConversion_result());
-                        System.out.println("By Jonathan Ortega");
-                        System.out.println("###########################33");
+                        z = valueUser;
+                        inputUser.nextLine(); // Consumir el carácter de nueva línea
                     }
                 }
             }
 
+            // Setting URL
+            String data = "https://v6.exchangerate-api.com/v6/3d3a8c46ed3aa968aa323588/pair/" + x + "/" + y + "/" + z;
 
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(data))
+                    .build();
 
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
+            // CODIFICACION DE DATOS
+            String json = response.body();
+            Gson gson = new Gson();
+            Conversion conversion = gson.fromJson(json, Conversion.class);
+            System.out.println("Conversion: " + x + " a " + y);
+            System.out.println("Resultado:  " + conversion.getConversion_result());
+            System.out.println("By Jonathan Ortega");
+            System.out.println("###########################");
 
         }
-
-
-        //JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
-
-
-        // Gson gson = new Gson();
-
-
-        //Setting Url
-
-
-        // System.out.println(response.body());
 
     }
 
